3a31f217a522cf67273e579095b0c1d5f6839310
Shivamjoshi902 <sj8630470355@gmail.com>
2024-03-10 19:48:41 +0530

    designing user and video model and using bcrypt to encrypt password

---
+++ src/models/user.models.js
@@ -0,0 +1,59 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from 'bcrypt'
+
+const userSchema = new Schema({
+    userName:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true,
+    },
+    avatar:{
+        type:String,
+        required:true,
+    },
+    coverImage:{
+        type:String,
+    },
+    watchHistory:{
+        type:mongoose.Types.ObjectId,
+        ref:'Video'
+    },
+    password:{
+        type:String,
+        required:[true,'password is required'],
+    },
+    refreshToken:{
+        type:String,
+    }
+
+},{timestamps:true})
+
+userSchema.pre("save",async function(next){
+    if(!(this.isModified('password'))) return next()
+
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare('password',this.password);
+}
+
+
+export const User=mongoose.model('User',userSchema);
